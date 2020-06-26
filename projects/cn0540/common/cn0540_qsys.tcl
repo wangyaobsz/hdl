amaslov86/adi-hdl

cn0540: Add de10nano reference design

projects/cn0540/common/cn0540_qsys.tcl
@@ -1,0 +1,113 @@
+
+    # dma
+
+    add_instance axi_dmac_0 axi_dmac
+    set_instance_parameter_value axi_dmac_0 {DMA_DATA_WIDTH_SRC} {32}
+    set_instance_parameter_value axi_dmac_0 {DMA_DATA_WIDTH_DEST} {64}
+    set_instance_parameter_value axi_dmac_0 {DMA_2D_TRANSFER} {0}
+    set_instance_parameter_value axi_dmac_0 {DMA_LENGTH_WIDTH} {24}
+    set_instance_parameter_value axi_dmac_0 {AXI_SLICE_DEST} {0}
+    set_instance_parameter_value axi_dmac_0 {AXI_SLICE_SRC} {0}
+    set_instance_parameter_value axi_dmac_0 {SYNC_TRANSFER_START} {0}
+    set_instance_parameter_value axi_dmac_0 {CYCLIC} {1}
+    set_instance_parameter_value axi_dmac_0 {DMA_TYPE_DEST} {0}
+    set_instance_parameter_value axi_dmac_0 {DMA_TYPE_SRC} {1}
+    set_instance_parameter_value axi_dmac_0 {FIFO_SIZE} {8}
+    set_instance_parameter_value axi_dmac_0 {HAS_AXIS_TLAST} {0}
+
+    add_connection axi_dmac_0.m_dest_axi sys_hps.f2h_sdram0_data
+    add_connection sys_dma_clk.clk axi_dmac_0.if_s_axis_aclk
+    add_connection sys_clk.clk axi_dmac_0.s_axi_clock
+    add_connection sys_clk.clk_reset axi_dmac_0.m_dest_axi_reset
+    add_connection sys_clk.clk_reset axi_dmac_0.s_axi_reset
+    add_connection sys_dma_clk.clk axi_dmac_0.m_dest_axi_clock
+    add_connection sys_dma_clk.clk_reset axi_dmac_0.m_dest_axi_reset
+
+	# create a SPI Engine architecture for ADC
+
+    # axi_spi_engine
+
+    add_instance axi_spi_engine_0 axi_spi_engine
+    set_instance_parameter_value axi_spi_engine_0 {ASYNC_SPI_CLK} {1}
+    set_instance_parameter_value axi_spi_engine_0 {DATA_WIDTH}    {32}
+    set_instance_parameter_value axi_spi_engine_0 {MM_IF_TYPE}    {0}
+    set_instance_parameter_value axi_spi_engine_0 {NUM_OF_SDI}    {1}
+    set_instance_parameter_value axi_spi_engine_0 {NUM_OFFLOAD}   {1}
+
+    add_connection sys_dma_clk.clk axi_spi_engine_0.if_spi_clk
+    add_connection sys_clk.clk axi_spi_engine_0.s_axi_clock
+    add_connection sys_clk.clk_reset axi_spi_engine_0.s_axi_reset
+
+    # spi_engine_execution
+
+    add_instance spi_engine_execution_0 spi_engine_execution
+    set_instance_parameter_value spi_engine_execution_0 {DATA_WIDTH} {32}
+    set_instance_parameter_value spi_engine_execution_0 {NUM_OF_SDI} {1}
+
+    add_connection sys_dma_clk.clk spi_engine_execution_0.if_clk
+    add_connection axi_spi_engine_0.if_spi_resetn spi_engine_execution_0.if_resetn
+    add_interface spi_engine_execution_0_if_cs conduit end
+    set_interface_property spi_engine_cs EXPORT_OF spi_engine_execution_0.if_cs
+    add_interface spi_engine_execution_0_if_sclk clock source
+    set_interface_property spi_engine_sclk EXPORT_OF spi_engine_execution_0.if_sclk
+    add_interface spi_engine_execution_0_if_sdi conduit end
+    set_interface_property spi_engine_sdi EXPORT_OF spi_engine_execution_0.if_sdi
+    add_interface spi_engine_execution_0_if_sdo conduit end
+    set_interface_property spi_engine_sdo EXPORT_OF spi_engine_execution_0.if_sdo
+    add_interface spi_engine_sdo_t conduit end
+    set_interface_property spi_engine_sdo_t EXPORT_OF spi_engine_execution_0.if_sdo_t
+
+    # spi_engine_interconnect
+
+    add_instance spi_engine_interconnect_0 spi_engine_interconnect
+    set_instance_parameter_value spi_engine_interconnect_0 {DATA_WIDTH} {32}
+    set_instance_parameter_value spi_engine_interconnect_0 {NUM_OF_SDI} {1}
+
+    add_connection sys_dma_clk.clk spi_engine_interconnect_0.if_clk
+    add_connection axi_spi_engine_0.if_spi_resetn spi_engine_interconnect_0.if_resetn
+
+    # spi_engine_offload
+
+    add_instance spi_engine_offload_0 spi_engine_offload
+    set_instance_parameter_value spi_engine_offload_0 {ASYNC_TRIG}    {1}
+    set_instance_parameter_value spi_engine_offload_0 {ASYNC_SPI_CLK} {1}
+    set_instance_parameter_value spi_engine_offload_0 {DATA_WIDTH}    {32}
+    set_instance_parameter_value spi_engine_offload_0 {NUM_OF_SDI}    {1}
+
+    add_connection sys_dma_clk.clk spi_engine_offload_0.if_ctrl_clk
+    add_connection sys_dma_clk.clk spi_engine_offload_0.if_spi_clk
+    add_connection axi_spi_engine_0.if_spi_resetn spi_engine_offload_0.if_spi_resetn
+    add_interface spi_engine_offload_0_if_trigger conduit end
+    set_interface_property spi_engine_trigger EXPORT_OF spi_engine_offload_0.if_trigger
+
+    # SPI Engine connections
+
+    add_connection axi_spi_engine_0.cmd spi_engine_interconnect_0.s0_cmd
+    add_connection axi_spi_engine_0.sdo_data spi_engine_interconnect_0.s0_sdo
+    add_connection spi_engine_execution_0.sdi_data spi_engine_interconnect_0.m_sdi
+    add_connection spi_engine_execution_0.sync spi_engine_interconnect_0.m_sync
+    add_connection spi_engine_interconnect_0.m_cmd spi_engine_execution_0.cmd
+    add_connection spi_engine_interconnect_0.m_sdo spi_engine_execution_0.sdo_data
+    add_connection spi_engine_interconnect_0.s0_sdi axi_spi_engine_0.sdi_data
+    add_connection spi_engine_interconnect_0.s0_sync axi_spi_engine_0.sync
+    add_connection spi_engine_interconnect_0.s1_sdi spi_engine_offload_0.sdi_data
+    add_connection spi_engine_interconnect_0.s1_sync spi_engine_offload_0.sync
+    add_connection spi_engine_offload_0.cmd spi_engine_interconnect_0.s1_cmd
+    add_connection spi_engine_offload_0.ctrl_cmd_wr axi_spi_engine_0.offload0_cmd
+    add_connection spi_engine_offload_0.ctrl_sdo_wr axi_spi_engine_0.offload0_sdo
+    add_connection spi_engine_offload_0.if_ctrl_enable axi_spi_engine_0.if_offload0_enable
+    add_connection spi_engine_offload_0.if_ctrl_enabled axi_spi_engine_0.if_offload0_enabled
+    add_connection spi_engine_offload_0.if_ctrl_mem_reset axi_spi_engine_0.if_offload0_mem_reset
+    add_connection spi_engine_offload_0.sdo_data spi_engine_interconnect_0.s1_sdo
+    add_connection spi_engine_offload_0.offload_sdi axi_dmac_0.s_axis
+
+    # cpu interconnects
+
+    ad_cpu_interconnect 0x00020000 axi_dmac_0.s_axi
+    ad_cpu_interconnect 0x00028000 axi_spi_engine_0.s_axi
+
+    #interrupts
+
+    ad_cpu_interrupt 3 axi_dmac_0.interrupt_sender
+    ad_cpu_interrupt 4 axi_spi_engine_0.interrupt_sender
+

projects/cn0540/de10nano/system_top.v
@@ -1,0 +1,236 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2019 - 2020 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsabilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module system_top (
+
+  // clock and resets
+
+  input            sys_clk,
+
+  // hps-ddr
+
+  output  [14:0]   ddr3_a,
+  output  [ 2:0]   ddr3_ba,
+  output           ddr3_reset_n,
+  output           ddr3_ck_p,
+  output           ddr3_ck_n,
+  output           ddr3_cke,
+  output           ddr3_cs_n,
+  output           ddr3_ras_n,
+  output           ddr3_cas_n,
+  output           ddr3_we_n,
+  inout   [31:0]   ddr3_dq,
+  inout   [ 3:0]   ddr3_dqs_p,
+  inout   [ 3:0]   ddr3_dqs_n,
+  output  [ 3:0]   ddr3_dm,
+  output           ddr3_odt,
+  input            ddr3_rzq,
+
+  // hps-ethernet
+
+  output            eth1_tx_clk,
+  output            eth1_tx_ctl,
+  output  [  3:0]   eth1_tx_d,
+  input             eth1_rx_clk,
+  input             eth1_rx_ctl,
+  input   [  3:0]   eth1_rx_d,
+  output            eth1_mdc,
+  inout             eth1_mdio,
+
+  // hps-sdio
+
+  output            sdio_clk,
+  inout             sdio_cmd,
+  inout   [  3:0]   sdio_d,
+
+  // hps-spim1
+
+  output            spim1_ss0,
+  output            spim1_clk,
+  output            spim1_mosi,
+  input             spim1_miso,
+
+  // hps-usb
+
+  input             usb1_clk,
+  output            usb1_stp,
+  input             usb1_dir,
+  input             usb1_nxt,
+  inout   [  7:0]   usb1_d,
+
+  // hps-uart
+
+  input             uart0_rx,
+  output            uart0_tx,
+
+  // board gpio
+
+  output  [  3:0]   gpio_bd_o,
+  input   [  7:0]   gpio_bd_i,
+
+  // cn0540
+
+  inout            i2c_sda,
+  inout            i2c_scl,
+
+  input            cn0540_spi_miso,
+  output           cn0540_spi_mosi,
+  output           cn0540_spi_sclk,
+  output           cn0540_spi_cs,
+  input            cn0540_drdy,
+
+  output           cn0540_reset_adc,
+  output           cn0540_shutdown,
+  output           cn0540_csb_aux,
+  output           cn0540_sw_ff,
+  output           cn0540_drdy_aux,
+  output           cn0540_blue_led,
+  output           cn0540_yellow_led,
+  input            cn0540_sync_in);
+
+  // internal signals
+
+  wire             sys_resetn;
+  wire    [63:0]   gpio_i;
+  wire    [63:0]   gpio_o;
+
+  // instantiations
+
+  assign gpio_i[63:41] = gpio_o[63:41];
+
+  assign cn0540_blue_led = gpio_o[40];
+  assign cn0540_yellow_led = gpio_o[39];
+  assign cn0540_sw_ff = gpio_o[38];
+  assign gpio_i[37] = cn0540_drdy;
+  assign cn0540_shutdown = gpio_o[36];
+  assign cn0540_drdy_aux = gpio_o[35];
+  assign cn0540_csb_aux = gpio_o[34];
+  assign gpio_i[33] = cn0540_sync_in;
+  assign cn0540_reset_adc = gpio_o[32];
+
+  assign gpio_i[31:12] = gpio_o[31:12];
+
+  assign gpio_i[11:4] = gpio_bd_i[7:0];
+  assign gpio_bd_o[3:0] = gpio_o[3:0];
+
+  // IO Buffers for I2C
+
+  wire i2c_0_scl_out;
+  wire i2c_0_scl_in;
+  wire i2c_0_sda_in;
+  wire i2c_0_sda_oe;
+
+  ALT_IOBUF scl_iobuf (.i(1'b0), .oe(i2c_0_scl_out), .o(i2c_0_scl_in), .io(i2c_scl));
+  ALT_IOBUF sda_iobuf (.i(1'b0), .oe(i2c_0_sda_oe), .o(i2c_0_sda_in), .io(i2c_sda));
+
+  system_bd i_system_bd (
+    .sys_clk_clk (sys_clk),
+    .sys_hps_h2f_reset_reset_n (sys_resetn),
+    .sys_hps_memory_mem_a (ddr3_a),
+    .sys_hps_memory_mem_ba (ddr3_ba),
+    .sys_hps_memory_mem_ck (ddr3_ck_p),
+    .sys_hps_memory_mem_ck_n (ddr3_ck_n),
+    .sys_hps_memory_mem_cke (ddr3_cke),
+    .sys_hps_memory_mem_cs_n (ddr3_cs_n),
+    .sys_hps_memory_mem_ras_n (ddr3_ras_n),
+    .sys_hps_memory_mem_cas_n (ddr3_cas_n),
+    .sys_hps_memory_mem_we_n (ddr3_we_n),
+    .sys_hps_memory_mem_reset_n (ddr3_reset_n),
+    .sys_hps_memory_mem_dq (ddr3_dq),
+    .sys_hps_memory_mem_dqs (ddr3_dqs_p),
+    .sys_hps_memory_mem_dqs_n (ddr3_dqs_n),
+    .sys_hps_memory_mem_odt (ddr3_odt),
+    .sys_hps_memory_mem_dm (ddr3_dm),
+    .sys_hps_memory_oct_rzqin (ddr3_rzq),
+    .sys_rst_reset_n (sys_resetn),
+    .sys_hps_hps_io_hps_io_emac1_inst_TX_CLK (eth1_tx_clk),
+    .sys_hps_hps_io_hps_io_emac1_inst_TXD0 (eth1_tx_d[0]),
+    .sys_hps_hps_io_hps_io_emac1_inst_TXD1 (eth1_tx_d[1]),
+    .sys_hps_hps_io_hps_io_emac1_inst_TXD2 (eth1_tx_d[2]),
+    .sys_hps_hps_io_hps_io_emac1_inst_TXD3 (eth1_tx_d[3]),
+    .sys_hps_hps_io_hps_io_emac1_inst_RXD0 (eth1_rx_d[0]),
+    .sys_hps_hps_io_hps_io_emac1_inst_MDIO (eth1_mdio),
+    .sys_hps_hps_io_hps_io_emac1_inst_MDC (eth1_mdc),
+    .sys_hps_hps_io_hps_io_emac1_inst_RX_CTL (eth1_rx_ctl),
+    .sys_hps_hps_io_hps_io_emac1_inst_TX_CTL (eth1_tx_ctl),
+    .sys_hps_hps_io_hps_io_emac1_inst_RX_CLK (eth1_rx_clk),
+    .sys_hps_hps_io_hps_io_emac1_inst_RXD1 (eth1_rx_d[1]),
+    .sys_hps_hps_io_hps_io_emac1_inst_RXD2 (eth1_rx_d[2]),
+    .sys_hps_hps_io_hps_io_emac1_inst_RXD3 (eth1_rx_d[3]),
+    .sys_hps_hps_io_hps_io_sdio_inst_CMD (sdio_cmd),
+    .sys_hps_hps_io_hps_io_sdio_inst_D0 (sdio_d[0]),
+    .sys_hps_hps_io_hps_io_sdio_inst_D1 (sdio_d[1]),
+    .sys_hps_hps_io_hps_io_sdio_inst_CLK (sdio_clk),
+    .sys_hps_hps_io_hps_io_sdio_inst_D2 (sdio_d[2]),
+    .sys_hps_hps_io_hps_io_sdio_inst_D3 (sdio_d[3]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D0 (usb1_d[0]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D1 (usb1_d[1]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D2 (usb1_d[2]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D3 (usb1_d[3]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D4 (usb1_d[4]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D5 (usb1_d[5]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D6 (usb1_d[6]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D7 (usb1_d[7]),
+    .sys_hps_hps_io_hps_io_usb1_inst_CLK (usb1_clk),
+    .sys_hps_hps_io_hps_io_usb1_inst_STP (usb1_stp),
+    .sys_hps_hps_io_hps_io_usb1_inst_DIR (usb1_dir),
+    .sys_hps_hps_io_hps_io_usb1_inst_NXT (usb1_nxt),
+    .sys_hps_hps_io_hps_io_uart0_inst_RX (uart0_rx),
+    .sys_hps_hps_io_hps_io_uart0_inst_TX (uart0_tx),
+    .sys_hps_hps_io_hps_io_spim1_inst_CLK (spim1_clk),
+    .sys_hps_hps_io_hps_io_spim1_inst_MOSI (spim1_mosi),
+    .sys_hps_hps_io_hps_io_spim1_inst_MISO (spim1_miso),
+    .sys_hps_hps_io_hps_io_spim1_inst_SS0 (spim1_ss0),
+    .sys_hps_i2c_0_sda (i2c_0_sda_in),
+    .sys_hps_i2c_0_out_data (i2c_0_sda_oe),
+    .sys_hps_i2c_0_scl_out_clk (i2c_0_scl_out),
+    .sys_hps_i2c_0_scl_in_clk (i2c_0_scl_in),
+    .sys_gpio_bd_in_port (gpio_i[31:0]),
+    .sys_gpio_bd_out_port (gpio_o[31:0]),
+    .sys_gpio_in_export (gpio_i[63:32]),
+    .sys_gpio_out_export (gpio_o[63:32]),
+    .spi_engine_sdo_sdo (cn0540_spi_mosi),
+    .spi_engine_sdo_t_sdo_t (),
+    .spi_engine_sdi_sdi (cn0540_spi_miso),
+    .spi_engine_cs_cs (cn0540_spi_cs),
+    .spi_engine_sclk_clk (cn0540_spi_sclk),
+    .spi_engine_trigger_trigger (cn0540_drdy));
+
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************

wangyaobsz/hdl

projects/cn0540/common/cn0540_qsys.tcl
@@ -1,0 +1,113 @@
+
+    # dma
+
+    add_instance axi_dmac_0 axi_dmac
+    set_instance_parameter_value axi_dmac_0 {DMA_DATA_WIDTH_SRC} {32}
+    set_instance_parameter_value axi_dmac_0 {DMA_DATA_WIDTH_DEST} {64}
+    set_instance_parameter_value axi_dmac_0 {DMA_2D_TRANSFER} {0}
+    set_instance_parameter_value axi_dmac_0 {DMA_LENGTH_WIDTH} {24}
+    set_instance_parameter_value axi_dmac_0 {AXI_SLICE_DEST} {0}
+    set_instance_parameter_value axi_dmac_0 {AXI_SLICE_SRC} {0}
+    set_instance_parameter_value axi_dmac_0 {SYNC_TRANSFER_START} {0}
+    set_instance_parameter_value axi_dmac_0 {CYCLIC} {1}
+    set_instance_parameter_value axi_dmac_0 {DMA_TYPE_DEST} {0}
+    set_instance_parameter_value axi_dmac_0 {DMA_TYPE_SRC} {1}
+    set_instance_parameter_value axi_dmac_0 {FIFO_SIZE} {8}
+    set_instance_parameter_value axi_dmac_0 {HAS_AXIS_TLAST} {0}
+
+    add_connection axi_dmac_0.m_dest_axi sys_hps.f2h_sdram0_data
+    add_connection sys_dma_clk.clk axi_dmac_0.if_s_axis_aclk
+    add_connection sys_clk.clk axi_dmac_0.s_axi_clock
+    add_connection sys_clk.clk_reset axi_dmac_0.m_dest_axi_reset
+    add_connection sys_clk.clk_reset axi_dmac_0.s_axi_reset
+    add_connection sys_dma_clk.clk axi_dmac_0.m_dest_axi_clock
+    add_connection sys_dma_clk.clk_reset axi_dmac_0.m_dest_axi_reset
+
+	# create a SPI Engine architecture for ADC
+
+    # axi_spi_engine
+
+    add_instance axi_spi_engine_0 axi_spi_engine
+    set_instance_parameter_value axi_spi_engine_0 {ASYNC_SPI_CLK} {1}
+    set_instance_parameter_value axi_spi_engine_0 {DATA_WIDTH}    {32}
+    set_instance_parameter_value axi_spi_engine_0 {MM_IF_TYPE}    {0}
+    set_instance_parameter_value axi_spi_engine_0 {NUM_OF_SDI}    {1}
+    set_instance_parameter_value axi_spi_engine_0 {NUM_OFFLOAD}   {1}
+
+    add_connection sys_dma_clk.clk axi_spi_engine_0.if_spi_clk
+    add_connection sys_clk.clk axi_spi_engine_0.s_axi_clock
+    add_connection sys_clk.clk_reset axi_spi_engine_0.s_axi_reset
+
+    # spi_engine_execution
+
+    add_instance spi_engine_execution_0 spi_engine_execution
+    set_instance_parameter_value spi_engine_execution_0 {DATA_WIDTH} {32}
+    set_instance_parameter_value spi_engine_execution_0 {NUM_OF_SDI} {1}
+
+    add_connection sys_dma_clk.clk spi_engine_execution_0.if_clk
+    add_connection axi_spi_engine_0.if_spi_resetn spi_engine_execution_0.if_resetn
+    add_interface spi_engine_execution_0_if_cs conduit end
+    set_interface_property spi_engine_cs EXPORT_OF spi_engine_execution_0.if_cs
+    add_interface spi_engine_execution_0_if_sclk clock source
+    set_interface_property spi_engine_sclk EXPORT_OF spi_engine_execution_0.if_sclk
+    add_interface spi_engine_execution_0_if_sdi conduit end
+    set_interface_property spi_engine_sdi EXPORT_OF spi_engine_execution_0.if_sdi
+    add_interface spi_engine_execution_0_if_sdo conduit end
+    set_interface_property spi_engine_sdo EXPORT_OF spi_engine_execution_0.if_sdo
+    add_interface spi_engine_sdo_t conduit end
+    set_interface_property spi_engine_sdo_t EXPORT_OF spi_engine_execution_0.if_sdo_t
+
+    # spi_engine_interconnect
+
+    add_instance spi_engine_interconnect_0 spi_engine_interconnect
+    set_instance_parameter_value spi_engine_interconnect_0 {DATA_WIDTH} {32}
+    set_instance_parameter_value spi_engine_interconnect_0 {NUM_OF_SDI} {1}
+
+    add_connection sys_dma_clk.clk spi_engine_interconnect_0.if_clk
+    add_connection axi_spi_engine_0.if_spi_resetn spi_engine_interconnect_0.if_resetn
+
+    # spi_engine_offload
+
+    add_instance spi_engine_offload_0 spi_engine_offload
+    set_instance_parameter_value spi_engine_offload_0 {ASYNC_TRIG}    {1}
+    set_instance_parameter_value spi_engine_offload_0 {ASYNC_SPI_CLK} {1}
+    set_instance_parameter_value spi_engine_offload_0 {DATA_WIDTH}    {32}
+    set_instance_parameter_value spi_engine_offload_0 {NUM_OF_SDI}    {1}
+
+    add_connection sys_dma_clk.clk spi_engine_offload_0.if_ctrl_clk
+    add_connection sys_dma_clk.clk spi_engine_offload_0.if_spi_clk
+    add_connection axi_spi_engine_0.if_spi_resetn spi_engine_offload_0.if_spi_resetn
+    add_interface spi_engine_offload_0_if_trigger conduit end
+    set_interface_property spi_engine_trigger EXPORT_OF spi_engine_offload_0.if_trigger
+
+    # SPI Engine connections
+
+    add_connection axi_spi_engine_0.cmd spi_engine_interconnect_0.s0_cmd
+    add_connection axi_spi_engine_0.sdo_data spi_engine_interconnect_0.s0_sdo
+    add_connection spi_engine_execution_0.sdi_data spi_engine_interconnect_0.m_sdi
+    add_connection spi_engine_execution_0.sync spi_engine_interconnect_0.m_sync
+    add_connection spi_engine_interconnect_0.m_cmd spi_engine_execution_0.cmd
+    add_connection spi_engine_interconnect_0.m_sdo spi_engine_execution_0.sdo_data
+    add_connection spi_engine_interconnect_0.s0_sdi axi_spi_engine_0.sdi_data
+    add_connection spi_engine_interconnect_0.s0_sync axi_spi_engine_0.sync
+    add_connection spi_engine_interconnect_0.s1_sdi spi_engine_offload_0.sdi_data
+    add_connection spi_engine_interconnect_0.s1_sync spi_engine_offload_0.sync
+    add_connection spi_engine_offload_0.cmd spi_engine_interconnect_0.s1_cmd
+    add_connection spi_engine_offload_0.ctrl_cmd_wr axi_spi_engine_0.offload0_cmd
+    add_connection spi_engine_offload_0.ctrl_sdo_wr axi_spi_engine_0.offload0_sdo
+    add_connection spi_engine_offload_0.if_ctrl_enable axi_spi_engine_0.if_offload0_enable
+    add_connection spi_engine_offload_0.if_ctrl_enabled axi_spi_engine_0.if_offload0_enabled
+    add_connection spi_engine_offload_0.if_ctrl_mem_reset axi_spi_engine_0.if_offload0_mem_reset
+    add_connection spi_engine_offload_0.sdo_data spi_engine_interconnect_0.s1_sdo
+    add_connection spi_engine_offload_0.offload_sdi axi_dmac_0.s_axis
+
+    # cpu interconnects
+
+    ad_cpu_interconnect 0x00020000 axi_dmac_0.s_axi
+    ad_cpu_interconnect 0x00030000 axi_spi_engine_0.s_axi
+
+    #interrupts
+
+    ad_cpu_interrupt 3 axi_dmac_0.interrupt_sender
+    ad_cpu_interrupt 4 axi_spi_engine_0.interrupt_sender
+

projects/cn0540/de10nano/system_top.v
@@ -1,0 +1,236 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2019 - 2020 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsabilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module system_top (
+
+  // clock and resets
+
+  input            sys_clk,
+
+  // hps-ddr
+
+  output  [14:0]   ddr3_a,
+  output  [ 2:0]   ddr3_ba,
+  output           ddr3_reset_n,
+  output           ddr3_ck_p,
+  output           ddr3_ck_n,
+  output           ddr3_cke,
+  output           ddr3_cs_n,
+  output           ddr3_ras_n,
+  output           ddr3_cas_n,
+  output           ddr3_we_n,
+  inout   [31:0]   ddr3_dq,
+  inout   [ 3:0]   ddr3_dqs_p,
+  inout   [ 3:0]   ddr3_dqs_n,
+  output  [ 3:0]   ddr3_dm,
+  output           ddr3_odt,
+  input            ddr3_rzq,
+
+  // hps-ethernet
+
+  output            eth1_tx_clk,
+  output            eth1_tx_ctl,
+  output  [  3:0]   eth1_tx_d,
+  input             eth1_rx_clk,
+  input             eth1_rx_ctl,
+  input   [  3:0]   eth1_rx_d,
+  output            eth1_mdc,
+  inout             eth1_mdio,
+
+  // hps-sdio
+
+  output            sdio_clk,
+  inout             sdio_cmd,
+  inout   [  3:0]   sdio_d,
+
+  // hps-spim1
+
+  output            spim1_ss0,
+  output            spim1_clk,
+  output            spim1_mosi,
+  input             spim1_miso,
+
+  // hps-usb
+
+  input             usb1_clk,
+  output            usb1_stp,
+  input             usb1_dir,
+  input             usb1_nxt,
+  inout   [  7:0]   usb1_d,
+
+  // hps-uart
+
+  input             uart0_rx,
+  output            uart0_tx,
+
+  // board gpio
+
+  output  [  7:0]   gpio_bd_o,
+  input   [  5:0]   gpio_bd_i,
+
+  // cn0540
+
+  inout            i2c_sda,
+  inout            i2c_scl,
+
+  input            cn0540_spi_miso,
+  output           cn0540_spi_mosi,
+  output           cn0540_spi_sclk,
+  output           cn0540_spi_cs,
+  input            cn0540_drdy,
+
+  output           cn0540_reset_adc,
+  output           cn0540_shutdown,
+  output           cn0540_csb_aux,
+  output           cn0540_sw_ff,
+  output           cn0540_drdy_aux,
+  output           cn0540_blue_led,
+  output           cn0540_yellow_led,
+  input            cn0540_sync_in);
+
+  // internal signals
+
+  wire             sys_resetn;
+  wire    [63:0]   gpio_i;
+  wire    [63:0]   gpio_o;
+
+  // instantiations
+
+  assign gpio_i[63:41] = gpio_o[63:41];
+
+  assign cn0540_blue_led = gpio_o[40];
+  assign cn0540_yellow_led = gpio_o[39];
+  assign cn0540_sw_ff = gpio_o[38];
+  assign gpio_i[37] = cn0540_drdy;
+  assign cn0540_shutdown = gpio_o[36];
+  assign cn0540_drdy_aux = gpio_o[35];
+  assign cn0540_csb_aux = gpio_o[34];
+  assign gpio_i[33] = cn0540_sync_in;
+  assign cn0540_reset_adc = gpio_o[32];
+
+  assign gpio_i[31:14] = gpio_o[31:14];
+
+  assign gpio_i[13:8] = gpio_bd_i[5:0];
+  assign gpio_bd_o[7:0] = gpio_o[7:0];
+
+  // IO Buffers for I2C
+
+  wire i2c_scl_out;
+  wire i2c_scl_in;
+  wire i2c_sda_in;
+  wire i2c_sda_oe;
+
+  ALT_IOBUF scl_iobuf (.i(1'b0), .oe(i2c_scl_out), .o(i2c_scl_in), .io(i2c_scl));
+  ALT_IOBUF sda_iobuf (.i(1'b0), .oe(i2c_sda_oe), .o(i2c_sda_in), .io(i2c_sda));
+
+  system_bd i_system_bd (
+    .sys_clk_clk (sys_clk),
+    .sys_hps_h2f_reset_reset_n (sys_resetn),
+    .sys_hps_memory_mem_a (ddr3_a),
+    .sys_hps_memory_mem_ba (ddr3_ba),
+    .sys_hps_memory_mem_ck (ddr3_ck_p),
+    .sys_hps_memory_mem_ck_n (ddr3_ck_n),
+    .sys_hps_memory_mem_cke (ddr3_cke),
+    .sys_hps_memory_mem_cs_n (ddr3_cs_n),
+    .sys_hps_memory_mem_ras_n (ddr3_ras_n),
+    .sys_hps_memory_mem_cas_n (ddr3_cas_n),
+    .sys_hps_memory_mem_we_n (ddr3_we_n),
+    .sys_hps_memory_mem_reset_n (ddr3_reset_n),
+    .sys_hps_memory_mem_dq (ddr3_dq),
+    .sys_hps_memory_mem_dqs (ddr3_dqs_p),
+    .sys_hps_memory_mem_dqs_n (ddr3_dqs_n),
+    .sys_hps_memory_mem_odt (ddr3_odt),
+    .sys_hps_memory_mem_dm (ddr3_dm),
+    .sys_hps_memory_oct_rzqin (ddr3_rzq),
+    .sys_rst_reset_n (sys_resetn),
+    .sys_hps_hps_io_hps_io_emac1_inst_TX_CLK (eth1_tx_clk),
+    .sys_hps_hps_io_hps_io_emac1_inst_TXD0 (eth1_tx_d[0]),
+    .sys_hps_hps_io_hps_io_emac1_inst_TXD1 (eth1_tx_d[1]),
+    .sys_hps_hps_io_hps_io_emac1_inst_TXD2 (eth1_tx_d[2]),
+    .sys_hps_hps_io_hps_io_emac1_inst_TXD3 (eth1_tx_d[3]),
+    .sys_hps_hps_io_hps_io_emac1_inst_RXD0 (eth1_rx_d[0]),
+    .sys_hps_hps_io_hps_io_emac1_inst_MDIO (eth1_mdio),
+    .sys_hps_hps_io_hps_io_emac1_inst_MDC (eth1_mdc),
+    .sys_hps_hps_io_hps_io_emac1_inst_RX_CTL (eth1_rx_ctl),
+    .sys_hps_hps_io_hps_io_emac1_inst_TX_CTL (eth1_tx_ctl),
+    .sys_hps_hps_io_hps_io_emac1_inst_RX_CLK (eth1_rx_clk),
+    .sys_hps_hps_io_hps_io_emac1_inst_RXD1 (eth1_rx_d[1]),
+    .sys_hps_hps_io_hps_io_emac1_inst_RXD2 (eth1_rx_d[2]),
+    .sys_hps_hps_io_hps_io_emac1_inst_RXD3 (eth1_rx_d[3]),
+    .sys_hps_hps_io_hps_io_sdio_inst_CMD (sdio_cmd),
+    .sys_hps_hps_io_hps_io_sdio_inst_D0 (sdio_d[0]),
+    .sys_hps_hps_io_hps_io_sdio_inst_D1 (sdio_d[1]),
+    .sys_hps_hps_io_hps_io_sdio_inst_CLK (sdio_clk),
+    .sys_hps_hps_io_hps_io_sdio_inst_D2 (sdio_d[2]),
+    .sys_hps_hps_io_hps_io_sdio_inst_D3 (sdio_d[3]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D0 (usb1_d[0]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D1 (usb1_d[1]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D2 (usb1_d[2]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D3 (usb1_d[3]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D4 (usb1_d[4]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D5 (usb1_d[5]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D6 (usb1_d[6]),
+    .sys_hps_hps_io_hps_io_usb1_inst_D7 (usb1_d[7]),
+    .sys_hps_hps_io_hps_io_usb1_inst_CLK (usb1_clk),
+    .sys_hps_hps_io_hps_io_usb1_inst_STP (usb1_stp),
+    .sys_hps_hps_io_hps_io_usb1_inst_DIR (usb1_dir),
+    .sys_hps_hps_io_hps_io_usb1_inst_NXT (usb1_nxt),
+    .sys_hps_hps_io_hps_io_uart0_inst_RX (uart0_rx),
+    .sys_hps_hps_io_hps_io_uart0_inst_TX (uart0_tx),
+    .sys_hps_hps_io_hps_io_spim1_inst_CLK (spim1_clk),
+    .sys_hps_hps_io_hps_io_spim1_inst_MOSI (spim1_mosi),
+    .sys_hps_hps_io_hps_io_spim1_inst_MISO (spim1_miso),
+    .sys_hps_hps_io_hps_io_spim1_inst_SS0 (spim1_ss0),
+    .sys_hps_i2c0_sda (i2c_sda),
+    .sys_hps_i2c0_out_data (i2c_out_data),
+    .sys_hps_i2c0_clk_clk (i2c_out_clk),
+    .sys_hps_i2c0_scl_in_clk (i2c_scl),
+    .sys_gpio_bd_in_port (gpio_i[31:0]),
+    .sys_gpio_bd_out_port (gpio_o[31:0]),
+    .sys_gpio_in_export (gpio_i[63:32]),
+    .sys_gpio_out_export (gpio_o[63:32]),
+    .spi_engine_sdo_sdo (cn0540_spi_mosi),
+    .spi_engine_sdo_t_sdo_t (),
+    .spi_engine_sdi_sdi (cn0540_spi_miso),
+    .spi_engine_cs_cs (cn0540_spi_cs),
+    .spi_engine_sclk_clk (cn0540_spi_sclk),
+    .spi_engine_trigger_trigger (cn0540_drdy));
+
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************